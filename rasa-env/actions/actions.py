fa4e7fa64aa19a2303bee0fd5cf2e9466985b9c3
lsm-5 <lsm5@cin.ufpe.br>
2021-08-20 23:41:17 -0300

    actionCuriosity

--- rasa-env/actions/actions.py
+++ rasa-env/actions/actions.py
@@ -1,6 +1,12 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(
+    ['127.0.0.1'],
+    port=9200
+)
 
 class ActionAirfareFind(Action):
     
@@ -14,3 +20,24 @@ class ActionAirfareFind(Action):
         
         dispatcher.utter_message(text="Achei o voo para você")
         return []
+
+class ActionCuriosity(Action):
+    
+    def name(self) -> Text:
+        return 'curiosity'
+    
+    def run(self, 
+            dispatcher: CollectingDispatcher, 
+            tracker: Tracker, 
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        message = tracker.latest_message.get('text')
+        result = searchCuriosity(message)
+
+        dispatcher.utter_message(text=result)
+
+        return []
+
+    def searchCuriosity(search):
+        res = es.search(index="python-pln-elasticsearch", body={"query": {"match": {"question": "" + search + ""}}})
+        return "\n{}".format(res['hits']['hits'][0]["_source"]["answer"])
