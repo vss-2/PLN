27f1c95f6e9acf11795149ef521f7086a2d9ae8b
felipebma <fbma@cin.ufpe.br>
2021-08-22 16:18:36 -0300

    Add grounds transportation flow

--- rasa-env/actions/actions.py
+++ rasa-env/actions/actions.py
@@ -2,6 +2,9 @@ from typing import Any, Text, Dict, List, Union
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+from datetime import datetime
+import requests
+import json
 
 from elasticsearch import Elasticsearch
 
@@ -64,5 +67,68 @@ class SubmitFlightForm(Action):
             dispatcher: CollectingDispatcher, 
             tracker: Tracker, 
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        request_url = "http://localhost:8080/flights"
+
+        headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+
+        data = {
+            "source": tracker.get_slot("from_city"),
+            "destiny": tracker.get_slot("destination_city"),
+            "departure_time": tracker.get_slot("time")
+        }
+
+        try:
+            response = requests.get(request_url, headers=headers, data=json.dumps(data))
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
         
+        response = json.loads(response.content)
+
+        for flight in response:
+            source = flight["source"]
+            destiny = flight["destiny"]
+            date = datetime.fromisoformat(flight["departure_time"]).strftime("%d/%m/%Y")
+            time = datetime.fromisoformat(flight["departure_time"]).strftime("%H:%M")
+            dispatcher.utter_message("- departure-city: {}, destination-city: {}, date: {}, time: {}".format(source, destiny, date, time))
+        
+
         return [SlotSet("from_city", None), SlotSet("destination_city", None), SlotSet("time", None)]
+
+
+class SubmitGroundForm(Action):
+    
+    def name(self) -> Text:
+        return 'submit_ground_form'
+
+    def run(self, 
+            dispatcher: CollectingDispatcher, 
+            tracker: Tracker, 
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        request_url = "http://localhost:8080/grounds"
+
+        headers = {
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "city": tracker.get_slot("ground_city")
+        }
+
+        try:
+            response = requests.get(request_url, headers=headers, data=json.dumps(data))
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            raise SystemExit(err)
+        
+        response = json.loads(response.content)
+
+        for ground in response:
+            dispatcher.utter_message("- {}".format(ground))
+        
+        return [SlotSet("ground_city", None)]
